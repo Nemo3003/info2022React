7e83c5b26da7a05616b9ab8a24cad77dee0a3034
Nemo3003 <ulimarian2000@gmail.com>
2022-05-19 12:10:21 -0300

    added some dynamic content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import Noticia from './Components/Noticia';
 import Expenseltem from './Components/Expenseltem';
 
 function App() {
+  const expenses = [
+    { title : 'Car Insurance', amount : 200, date : new Date(2020,7,14) },
+    { title : 'Rent', amount : 1000 , date : new Date(2022,9,11)},
+    { title : 'Coffee', amount : 2 , date : new Date(2023,6,14)},
+    { title : 'Restaurant', amount : 10, date : new Date(2021,4,14) },
+    { title : 'Gym', amount : 50 , date : new Date(2024,2,14)}
+  ]
+
   return (
     <div className="App">
       {/* <header className="App-header">
@@ -21,7 +28,11 @@ function App() {
         </a>
       </header> */}
       <Noticia />
-      <Expenseltem />
+      <Expenseltem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></Expenseltem>
+      <Expenseltem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></Expenseltem>
+      <Expenseltem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></Expenseltem>
+      <Expenseltem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></Expenseltem>
+      <Expenseltem title={expenses[4].title} amount={expenses[4].amount} date={expenses[4].date}></Expenseltem>
     </div>
   );
 }
--- src/Components/Expenseltem.js
+++ src/Components/Expenseltem.js
@@ -1,11 +1,19 @@
 import './ExpenseItem.css'
 
-function Expenseltem(){
+function Expenseltem(props){
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+    const year = props.date.getFullYear();
     return (
     <div className='expense-item'>
-        <div>Date</div>
-            <div className='expense-item__description'><h2>Title</h2>
-            <div className='expense-item__price'>$246.36</div>
+        <div>
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{day}</div>
+        </div>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+            <div className='expense-item__price'>{props.amount}</div>
             </div>
     </div>
     );
